aef052fd7e67b861f9350744ee7af7687cc8212f
sunil kumar <sunilssaharan@gmail.com>
2016-12-20 07:17:16 +0000

    declared a node pointer node for first character and an node pointer loca for current location

--- dictionary.c
+++ dictionary.c
@@ -32,12 +32,26 @@ bool load(const char* dictionary)
         struct node* chil[27];            // 27 due to 26 characters and apostrophe
     }
     node;
+    node* root = malloc(sizeof(node));
     
+    // stores  current location in loca and remeber character number in index
+    node* loca;
+    index=0;
     // opening the dictionary file for loading.
     FILE* fp = fopen(dictionary,"r");
     for(int c=fgetc(fp);c != EOF;c=fgetc(fp))
     {
-        
+        // first character of word should go to root
+        if(isalpha(c)&&index=0)
+        {
+            int num=(c-'a');
+            node->children[num]=malloc(sizeof(node));
+            loca=node->children[num];
+        }
+        else if(isalpha(c))
+        {
+            
+        }
     }
     return false;
 }
